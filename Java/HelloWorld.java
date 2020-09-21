24c55ada1108e59f8be74f09849e7c07e4cde3e9
NEURON <50287102+neuron909@users.noreply.github.com>
2020-09-21 22:42:49 +0530

    Added: Hello World in Lang: c, cpp, java, python, js

---
+++ Java/HelloWorld.java
@@ -0,0 +1,18 @@
+// public class HelloWorld {
+
+//     public static void main ( String [] args ) {
+
+//         System.out.println( "Hello World!" );
+
+//     }
+// }
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        String menu[] = { "Add Payee", "List All Payee", "Send Money", "Exit" };
+    }
+}
