d5b14fe2065d0eee19447418833f5784c3a04840
Sasuke-003 <56964273+Sasuke-003@users.noreply.github.com>
2020-10-10 10:38:09 +0530

    Added: LargestOfNumbers, Fibonacci in { Java }

---
+++ Java/LargestOfNumbers.java
@@ -0,0 +1,33 @@
+public class LargestOfNumbers {
+    static int largestOfTwo( int num1, int num2 ) {
+        if( num1 > num2 )
+            return num1;
+        return num2;
+    }
+
+    static int largestOfThree( int num1, int num2, int num3 ) {
+        // Complete the function
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int n1 = 50;
+        int n2 = 400;
+        int n3 = -10;
+        int n4 = 500;
+        int n5 = -50;
+
+        System.out.println( "Largest among n1, n2 : "+largestOfTwo( n1, n2 ) ) ;
+
+        /* Please create below functions to find the largest among the numbers */
+
+        // System.out.println( "Largest among n1, n2, n3 : "+largestOfThree( n1, n2, n3 ) ) ;
+        // System.out.println( "Largest among n1, n2, n3, n4 : "+largestOfFour( n1, n2, n3, n4 ) ) ;
+        // System.out.println( "Largest among n1, n2, n3, n4, n5 : "+largestOfFive( n1, n2, n3, n4, n5 ) ) ;
+        
+    }
+
+}
