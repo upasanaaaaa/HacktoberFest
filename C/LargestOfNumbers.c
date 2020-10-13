9dfb9d92ee542927a2c132018ca506bddefc62a1
Deeksha1521 <70743065+Deeksha1521@users.noreply.github.com>
2020-10-13 18:15:08 +0530

    Update LargestOfNumbers.c
    Added function for largestOfFour
    Added function for largestOfFive

--- C/LargestOfNumbers.c
+++ C/LargestOfNumbers.c
@@ -11,7 +11,12 @@ int largestOfThree( int num1, int num2, int num3 ) {
     return largestOfTwo( largestOfTwo( num1, num2), num3 ));
 
 }
-
+int largestOfFour(int num1, int num2, int num3, int num4) {
+    return largestOfTwo(largestOfThree(num1, num2, num3), num4)) ;
+}
+int largestOfFive(int num1, int num2, int num3, int num4, int num5) {
+    return largestOfTwo(largestOfFour(num1, num2, num3, num4) , num5)) ;
+    }
 void main() {
 
     int n1 = 50;
@@ -25,7 +30,7 @@ void main() {
     /* Please create below functions to find the largest among the numbers */
 
     printf( "Largest among n1, n2, n3 : %d\n", largestOfThree( n1, n2, n3 ) ) ;
-    // printf( "Largest among n1, n2, n3, n4 : %d\n", largestOfFour( n1, n2, n3, n4 ) ) ;
-    // printf( "Largest among n1, n2, n3, n4, n5 : %d\n", largestOfFive( n1, n2, n3, n4, n5 ) ) ;
+    printf( "Largest among n1, n2, n3, n4 : %d\n", largestOfFour( n1, n2, n3, n4 ) ) ;
+    printf( "Largest among n1, n2, n3, n4, n5 : %d\n", largestOfFive( n1, n2, n3, n4, n5 ) ) ;
     
 }
