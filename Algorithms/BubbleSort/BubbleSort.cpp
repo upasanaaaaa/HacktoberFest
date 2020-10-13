5d4e316327b509086ac77a73766dc839b7e4eafe
Dattha Prasad <57026609+datthaprasad@users.noreply.github.com>
2020-10-13 17:15:52 +0530

    Update BubbleSort.cpp
    bubble sort function is added

--- Algorithms/BubbleSort/BubbleSort.cpp
+++ Algorithms/BubbleSort/BubbleSort.cpp
@@ -1,13 +1,23 @@
 #include <bits/stdc++.h> 
 using namespace std; 
   
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
   
+// A function to implement bubble sort  
 void bubbleSort(int arr[], int n)  
 {  
-    for i in range(n): 
-        for j in range(0, n-i-1): 
-            if arr[j] > arr[j+1] : 
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+      
+    // Last i elements are already in place  
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1])  
+            swap(&arr[j], &arr[j+1]);  
 }  
   
 /* Function to print an array */
