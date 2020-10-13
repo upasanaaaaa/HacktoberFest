729986676e41f671ab36beca69974cdb6db4d7ad
Dattha Prasad <57026609+datthaprasad@users.noreply.github.com>
2020-10-13 21:37:48 +0530

    Update BubbleSort.js
    added function to bubblesort

--- Algorithms/BubbleSort/BubbleSort.js
+++ Algorithms/BubbleSort/BubbleSort.js
@@ -1,7 +1,16 @@
 
 function bubble_Sort(arr){
 
-//complete the function to implement bubble sort
+    let len = arr.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
     return arr;
 }
 console.log(bubble_Sort([3, 0, 2, 5, -1, 4, 1]));
