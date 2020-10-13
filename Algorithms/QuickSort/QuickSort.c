c3371a6e373b29a002fb7bc3aa504541c7913771
Hariprasad004 <57026806+Hariprasad004@users.noreply.github.com>
2020-10-13 17:31:31 +0530

    Updated Quicksort.c

--- Algorithms/QuickSort/QuickSort.c
+++ Algorithms/QuickSort/QuickSort.c
@@ -1,67 +1,67 @@
 /* C implementation QuickSort */
  // There is some Bug in the below code find and fix it
-#include<stdio.h> 
+#include<stdio.h>
 
-// A utility function to swap two elements 
-void swap(int* a, int* b) 
-{ 
-	int t = *a; 
-	*a = *b; 
-	*b = t; 
-} 
-
-
-int partition (int arr[], int low, int high) 
-{ 
-	int pivot = arr[high]; // pivot 
-	int i = (low - 1); // Index of smaller element 
-
-	for (int j = low; j <= high- 1; j++) 
-	{ 
-		// If current element is smaller than the pivot 
-		if (arr[j] < pivot) 
-		{ 
-			i++; // increment index of smaller element 
-			swap(&arr[i], &arr[j]); 
-		} 
-	} 
-	swap(&arr[i + 1], &arr[high]); 
-	return (i + 1); 
-} 
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
 
 
-void quickSort(int arr[], int low, int high) 
-{ 
+int partition (int arr[], int low, int high)
+{
+	int pivot = arr[high]; // pivot
+	int i = (low - 1); // Index of smaller element
 
-	if (low < high) 
-	{ 
-		/* pi is partitioning index, arr[p] is now 
+	for (int j = low; j <= high- 1; j++)
+	{
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot)
+		{
+			i++; // increment index of smaller element
+			swap(&arr[i], &arr[j]);
+		}
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+
+void quickSort(int arr[], int low, int high)
+{
+
+	if (low < high)
+	{
+		/* pi is partitioning index, arr[p] is now
 		at right place */
-		int pi = partition(arr, high, low); 
+		int pi = partition(arr, low, high);
 
-		// Separately sort elements before 
-		// partition and after partition 
-		quickSort(arr, high, pi ); 
-		quickSort(arr, pi , low); 
-	} 
-} 
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi-1 );
+		quickSort(arr, pi+1 , high);
+	}
+}
 
 /* Function to print an array */
-void printArray(int arr[], int size) 
-{ 
-	int i; 
-	for (i=0; i < size; i++) 
-		printf("%d ", arr[i]); 
-	printf("\n"); 
-} 
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i=0; i < size; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
 
 
-int main() 
-{ 
-	int arr[] = {10, 7, 8, 9, 1, 5}; 
-	int n = sizeof(arr)/sizeof(arr[0]); 
-	quickSort(arr, 0, n-1); 
-	printf("Sorted array: \n"); 
-	printArray(arr, n); 
-	return 0; 
-} 
+int main()
+{
+	int arr[] = {10, 7, 8, 9, 1, 5};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	quickSort(arr, 0, n-1);
+	printf("Sorted array: \n");
+	printArray(arr, n);
+	return 0;
+}
