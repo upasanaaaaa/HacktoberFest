78718815efd7f38ee55aa14b0e3ccd5474385de0
Dattha Prasad <57026609+datthaprasad@users.noreply.github.com>
2020-10-14 21:21:38 +0530

    Update BubbleSort.py
    implemented bubblesort program in python

--- Algorithms/BubbleSort/BubbleSort.py
+++ Algorithms/BubbleSort/BubbleSort.py
@@ -1,6 +1,14 @@
 def bubbleSort(arr):
-    # complete the function to implement bubble sort
-    pass
+    n = len(arr) 
+  
+    # Traverse through all array elements 
+    for i in range(n-1): 
+    # range(n) also work but outer loop will repeat one time more than needed. 
+  
+        # Last i elements are already in place 
+        for j in range(0, n-i-1): 
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
 arr = [100, 75, 25, 150, 10, 45, 32, 99]
