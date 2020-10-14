69511eaf7ccb79038b8fc427df9018b1568aa41a
CHARITH <charithchathra@gmail.com>
2020-10-14 12:08:16 +0530

    largest of 3 numbers is added

--- C++/LargestOfNumbers.cpp
+++ C++/LargestOfNumbers.cpp
@@ -7,9 +7,13 @@ int largestOfTwo( int num1, int num2 ) {
     return num2;
 }
 
-int largestOfThree( int num1, int num2, int num3 ) {
-    // Complete the function
-    return largestOfTwo( largestOfTwo( num1, num2), num3 )
+void largestOfThree( int n1, int n2, int n3 ) {
+     if((n1 >= n2) && (n1 >= n3))
+        cout << n1<<endl;
+    else if ((n2 >= n1) && (n2 >= n3))
+        cout<< n2<<endl;
+    else
+        cout<< n3<<endl;
 }
 
 int main() {
@@ -20,11 +24,12 @@ int main() {
     int n4 = 500;
     int n5 = -50;
 
-    cout << "Largest among n1, n2 : " << largestOfTwo( n1, n2 ) ;
+    cout << "Largest among n1, n2 : " << largestOfTwo( n1, n2 )<<endl ;
 
     /* Please create below functions to find the largest among the numbers */
 
-    cout << "\nLargest among n1, n2, n3 : " << largestOfThree( n1, n2, n3 ) ;
+    cout << "\nLargest among n1, n2, n3 : " ;
+    largestOfThree( n1, n2, n3 ) ;
     // cout << "Largest among n1, n2, n3, n4 : " << largestOfFour( n1, n2, n3, n4 ) ;
     // cout << "Largest among n1, n2, n3, n4, n5 : " << largestOfFive( n1, n2, n3, n4, n5 ) ;
     return 0;
