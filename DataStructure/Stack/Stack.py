5d0a7be32331e200369b849f6c73f389fd43814f
NEURON <50287102+neuron909@users.noreply.github.com>
2020-10-14 11:35:48 +0530

    Added: Queue and Stack in { c, js, java, py }

---
+++ DataStructure/Stack/Stack.py
@@ -0,0 +1,10 @@
+# Note:
+
+# Keep the usage of in-built function to the minimum
+# addOneEle/Optimize  features
+# Please follow best coding practices
+# addOneEle comments if necessary
+# And make the Code Bug free
+
+
+# Please refer Stack.cpp program and code in python
