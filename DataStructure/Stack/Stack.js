7126aab9aad37cd4788ca0ebc1cc125a48d58399
AnudeeP-1 <62413866+AnudeeP-1@users.noreply.github.com>
2020-10-15 13:04:46 +0530

    Update Stack.js
    updated once again

--- DataStructure/Stack/Stack.js
+++ DataStructure/Stack/Stack.js
@@ -9,5 +9,60 @@ And make the Code Bug free
 
 */
 
+// Stack class 
+class Stack { 
+
+	// Array is used to implement stack 
+	constructor() 
+	{ 
+		this.items = []; 
+	} 
+
+// push function 
+push(element) 
+{ 
+	// push element into the items 
+    this.items.push(element); 
+    console.log("Pushed: ",element);
+} 
+
+// pop function 
+pop() 
+{ 
+	// return top most element in the stack 
+	if (this.items.length == 0) 
+        return "Underflow"; 
+	return this.items.pop(); 
+} 
+
+// isEmpty function 
+isEmpty() 
+{ 
+	// return true if stack is empty 
+	return this.items.length == 0; 
+} 
+
+// printStack function 
+printStack() 
+{ 
+    console.log("Stack: ");
+	var str = ""; 
+	for (var i = this.items.length-1; i >= 0; i--) 
+		str += this.items[i] + " "; 
+	return str; 
+} 
+}
+
+// creating object for stack class 
+var stack = new Stack(); 
+
+// Adding element to the stack 
+console.log(stack.printStack());
+stack.push(10); 
+console.log(stack.printStack());
+stack.push(20); 
+console.log(stack.printStack());
+console.log("Popped: ",stack.pop()); 
+console.log(stack.printStack());
 
 //Please refer Stack.cpp program and code in js
